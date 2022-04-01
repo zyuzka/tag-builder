1957666cd0e6ed1947b639e4f2f8b16dc7e5c906
Filip Sushko <filip.sushko@spryker.com>
2022-04-01 12:17:05 +0300

    revert

--- index.php
+++ index.php
@@ -8,10 +8,4 @@ const APPLICATION_ROOT = __DIR__ . '/../../';
 
 $action = $argv[1] ?? '';
 
-try {
-    (new App())->run($action);
-} catch (Exception $e) {
-    echo $e->getMessage() . PHP_EOL;
-
-    exit(1);
-}
+(new App())->run($action);
